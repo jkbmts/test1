6131c16f11cec4c1d5b3d22653107ac16d669680
jkbmts <jakub.matusiak@student.wat.edu.pl>
2021-10-22 18:15:27 +0200

    calc2

---
+++ kalkulator/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 8;
+    int b = 4;
+    int result = a/b;
+    printf("Liczba a = %d\n", a);
+    printf("Liczba b = %d\n", b);
+    printf("Wynikiem dzielenia liczb a = %d i b = %d jest liczba %d\n", a, b, result);
+    printf("Koniec");
+    return 0;
+}
